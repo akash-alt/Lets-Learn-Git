45a079685756baf15eb89979e061cdb18a246e63
Akash Yadav <akashrequired@gmail.com>
2022-03-14 16:16:51 +0530

    added few changes div.js

--- div.js
+++ div.js
@@ -1,1 +1,10 @@
-console.log("hello git")
+
+for (let i=0;i<=25;i++){
+    if(i%3 ===0){
+        console.log('fizz')
+    }else if(i%5 ===0){
+        console.log("fizzbuzz")
+    }else(
+        console.log(i)
+    )
+}
